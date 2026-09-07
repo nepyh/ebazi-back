745401b7eadfdaf0989583a5e4a632a58558edf2
rhtnddd <qlalfql123@gmail.com>
2026-09-08 08:50:17 +0900

    fix: SchedulerJob 을 이름 없이 여러 모듈에서 등록하면 Koin이 서로 덮어써서 getAll()에 하나만 잡히던 버그 수정 (named 큐레이터 추가)

--- src/main/kotlin/com/github/nepyh/rooter/module/taskquiz/TaskQuizModule.kt
+++ src/main/kotlin/com/github/nepyh/rooter/module/taskquiz/TaskQuizModule.kt
@@ -9,7 +9,9 @@ import org.koin.dsl.module
 fun TaskQuizModule(appConfig: AppConfig) = module {
     single { TaskQuizLlmClient(appConfig) }
     single { TaskQuizService(get()) }
-    single<SchedulerJob> { TaskQuizTriggerJob(get()) }
+    // named 필수: SchedulerJob 을 이름 없이 등록하면 다른 모듈의 SchedulerJob 정의를 덮어써서
+    // SchedulerEngine 의 getAll<SchedulerJob>() 에 하나만 잡히는 문제가 생김
+    single<SchedulerJob>(named("taskQuizTriggerJob")) { TaskQuizTriggerJob(get()) }
 
     single(named("taskQuizApi")) { TaskQuizApi(get()) }
 }
